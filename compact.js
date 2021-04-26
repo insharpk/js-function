c1d040fc72aefb0ee0ecec97c7723fee89a1c5fc
insharpk <83079502+insharpk@users.noreply.github.com>
2021-04-27 01:30:28 +0900

    配列からfalsyな値を取り除いた配列を返す

---
+++ compact.js
@@ -0,0 +1,12 @@
+function compact(array){
+    const compactedArray=[];
+
+    for(let i=0;i<array.length;i++){
+        if(array[i]){
+            compactedArray.push(array[i]);
+        }
+    }
+
+    return compactedArray;
+}
+
