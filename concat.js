ff671b70ecb2c593aec9850f343bacdb5b0e18df
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-11 23:49:15 +0900

    Update concat.js

--- concat.js
+++ concat.js
@@ -1,1 +1,17 @@
-
+function concat(array,...values){
+  const newArray=[...array];
+  
+  for(let i=0;i<values.length;i++)
+  {
+       const value=values[i];
+    
+       if(Array.isArray(value)){
+           newArray.push(...value);    
+       }else{
+           newArray.push(value);
+       }
+    
+  }
+  
+  return newArray;
+}
