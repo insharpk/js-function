f74beedbf5aa85d884e9bb155c0ee6a288c7fcee
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-12 23:28:36 +0900

    Update pullAll.js

--- pullAll.js
+++ pullAll.js
@@ -1,1 +1,13 @@
 
+function pullAll (array,values)
+{
+    for(let i=array.length-1;0<=i;i--)
+    {
+      if(values.includes(array[i]))
+         {
+            array.splice(i,1);
+         }
+    }
+  
+    return array;
+}
