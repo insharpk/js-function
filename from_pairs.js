2b247a62fb633cb694e92c0975678c84ef027816
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-15 21:21:35 +0900

    Update from_pairs.js

--- from_pairs.js
+++ from_pairs.js
@@ -1,1 +1,10 @@
-
+function fromPairs(pairs){
+  const keyValuePair={};
+  
+  for(let i=0;i<pairs.length;i++){
+    const [key,value]=pairs[i];
+    keyValuePair[key]=value;
+  }
+  
+  return keyValuePair;
+}
