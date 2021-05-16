f020454c7ee88327ed4857f5721fcfc5a4b5e426
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-16 23:19:51 +0900

    Update takeRight.js

--- takeRight.js
+++ takeRight.js
@@ -1,1 +1,17 @@
 
+function takeRight(array,n=1){
+  if(n==0){
+  return [];
+  }
+  if(n>array.length){
+  return [...array];
+  }
+  const takenArray[];
+  
+  for(let i=0;i<array.length;i++){
+    const indexFromRight=array.length-(1+i);
+    takenArray.unshift(array[indexFromRight]);
+  }
+  
+  return takenArray;
+}
