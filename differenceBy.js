25e40c00b26e374cc73c66dfcfd85f7340fc3a9c
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-17 22:04:44 +0900

    Update differenceBy.js

--- differenceBy.js
+++ differenceBy.js
@@ -4,7 +4,7 @@ function differnceBy(targetValues,compareValues,iteratee)
     const diffValues=[];
 
     const processedCompareValues=compareValues.map(v => iteratee(v));
-    for(let i=0;i<array.length;i++)
+    for(let i=0;i<targetValues.length;i++)
     {
         const target=targetValues[i];
         const processedTarget=iteratee(target);
