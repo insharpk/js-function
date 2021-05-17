53fa9108bce567490bf4aafeb7bb935b3c9147af
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-17 22:01:39 +0900

    Update differenceBy.js

--- differenceBy.js
+++ differenceBy.js
@@ -1,1 +1,16 @@
 
+function differnceBy(targetValues,compareValues,iteratee)
+{   
+    const diffValues=[];
+
+    const processedCompareValues=compareValues.map(v => iteratee(v));
+    for(let i=0;i<array.length;i++)
+    {
+        const target=targetValues[i];
+        const processedTarget=iteratee(target);
+        if(!processedCompareValues.includes(processedTarget)){
+            diffValues.push(target);
+        }
+    }
+    return diffValues;
+}
