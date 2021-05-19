1517761fa82059e617a634f995b768dae13247d8
insharpk <83079502+insharpk@users.noreply.github.com>
2021-05-19 23:52:42 +0900

    Update findIndex.js

--- findIndex.js
+++ findIndex.js
@@ -1,5 +1,5 @@
 findIndex(array,predicate,fromIndex=0){
-  for(let i=0;i<array.length;i++){
+  for(let i=fromIndex;i<array.length;i++){
     const value =array[i]
     if(predicate(value)){
       return i;
@@ -7,3 +7,16 @@ findIndex(array,predicate,fromIndex=0){
   }
   return -1;
 }
+
+/*
+const users = [
+  { 'user': 'barney',  'active': false },
+  { 'user': 'fred',    'active': false },
+  { 'user': 'pebbles', 'active': true }
+];
+
+const result1 = findIndex(users, function(o) {
+  return o.user === 'barney';
+});
+console.log(result1);
+*/
